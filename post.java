21f23c00349b2a9d1c51d10a334b3635339989b2
Sadhanachowdary <41729636+Sadhanachowdary@users.noreply.github.com>
2018-07-26 15:45:32 +0530

    Create post.java

---
+++ post.java
@@ -0,0 +1,26 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class post
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+        int num;
+        Scanner sc=new Scanner(System.in);
+         
+        System.out.print("Enter any integer number: ");
+        num=sc.nextInt();
+        if(num>0)
+            System.out.println(num + " is POSITIVE NUMBER.");
+        else if(num<0)
+            System.out.println(num + " is NEGATIVE NUMBER.");
+        else
+            System.out.println("IT's ZERO.");
+         
+
+	}
+}
